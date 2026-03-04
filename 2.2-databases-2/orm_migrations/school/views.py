fdb46f159a97ef0413e4b25315aa81858952b811
Denis Ergashev <unique94denis@yandex.ru>
2026-03-04 20:49:38 +0300

    Выполнено задание: ManyToManyField, данные созданы через админку

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -1,15 +1,16 @@
-from django.views.generic import ListView
 from django.shortcuts import render
-
 from .models import Student
 
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
 
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
+    # Получаем всех учеников с сортировкой по классу
+    # prefetch_related оптимизирует запросы к базе данных
+    students = Student.objects.all().order_by('group').prefetch_related('teachers')
+
+    context = {
+        'object_list': students,
+    }
 
     return render(request, template, context)
