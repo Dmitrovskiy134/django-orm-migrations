7f9acccc4c7df14ab14e4245808f2e6118c33002
Denis Ergashev <unique94denis@yandex.ru>
2026-03-04 23:19:50 +0300

    Выполнено второе задание

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,55 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+
+        # Считаем количество основных тегов
+        main_count = 0
+        for form in self.forms:
+            # Пропускаем пустые формы (которые пользователь не заполнял)
+            if not form.cleaned_data or form.cleaned_data.get('DELETE'):
+                continue
+
+            if form.cleaned_data.get('is_main'):
+                main_count += 1
+
+        # Проверяем, что есть ровно один основной тег
+        if main_count == 0:
+            raise ValidationError('Укажите основной раздел')
+        elif main_count > 1:
+            raise ValidationError('Основным может быть только один раздел')
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 1  # сколько пустых форм показывать
+    autocomplete_fields = ['tag']  # удобный поиск тегов
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeInline]
+    list_display = ['title', 'published_at', 'display_tags']
+    list_filter = ['published_at']
+    search_fields = ['title', 'text']
+
+    def display_tags(self, obj):
+        """Отображает теги статьи в списке"""
+        tags = [scope.tag.name for scope in obj.scopes.all()]
+        return ", ".join(tags)
+
+    display_tags.short_description = 'Разделы'
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    ordering = ['name']
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,16 +1,46 @@
 from django.db import models
 
 
-class Article(models.Model):
+class Tag(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название')
 
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+        ordering = ['name']  # сортировка по алфавиту
+
+    def __str__(self):
+        return self.name
+
+
+class Scope(models.Model):
+    article = models.ForeignKey('Article', on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
+
+    class Meta:
+        verbose_name = 'Связь статьи и раздела'
+        verbose_name_plural = 'Связи статей и разделов'
+        # Чтобы не было дубликатов: одна статья + один тег = уникальная связь
+        unique_together = ('article', 'tag')
+
+    def __str__(self):
+        return f'{self.article.title} - {self.tag.name} ({"основной" if self.is_main else "дополнительный"})'
+
+
+class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
+
+    # Связь с тегами через промежуточную модель Scope
+    tags = models.ManyToManyField(Tag, through=Scope, related_name='articles')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']  # сортировка по дате (свежие сверху)
 
     def __str__(self):
         return self.title
